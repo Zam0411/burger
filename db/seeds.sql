1bb7689f368ea6a3df0e897f54a718e9c0f47bd8
Zam0411 <mzambrano41165@gmail.com>
2019-03-28 14:31:50 -0700

    HW

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheese Burger");
+INSERT INTO burgers (burger_name) VALUES ("Double Bacon Cheese Burger");
+INSERT INTO burgers (burger_name) VALUES ("Western Burger");
+INSERT INTO burgers (burger_name) VALUES ("Swiss Mushroom Angus Beef Burger");
